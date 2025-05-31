0d37b8cb8eeabae5c92783dc3e89d58470857c2c
BrasilicioHenrique <brasilicio.henrique@escolar.ifrn.edu.br>
2025-05-31 20:47:34 -0300

    melhorias

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 let count = 0;
 
 function addDado() {
+    if (count == 0) {
+        document.getElementById("rolagens").innerHTML += `<br>
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroup-sizing-default">Número a ser retirado</span>
+            </div>
+            <input id="numRetirado" type="number" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+        </div>`;
+    }
     document.getElementById("rolagens").innerHTML += `<br>  
-    <div class="input-group mb-3">
+    <div class="input-group">
         <div class="input-group-prepend">
             <span class="input-group-text" id="inputGroup-sizing-default">Lados do Dado</span>
         </div>
         <input id="numLados${count}" type="number" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
     </div>
-    <div class="input-group mb-3">
+    <div class="input-group mb-1">
         <div class="input-group-prepend">
             <span class="input-group-text" id="inputGroup-sizing-default">Qtd de Dados</span>
         </div>
@@ -25,10 +34,26 @@ function rolarDados(id) {
     const numLados = Number(document.getElementById(`numLados${id}`).value);
     const qtdDados = Number(document.getElementById(`qtdDados${id}`).value);
 
+    const numRetirado = Number(document.getElementById(`numRetirado`).value);
+
+    if ((numLados == "" || numLados <= 1) || (qtdDados == "" || qtdDados < 1) || (numLados > 2**100 || qtdDados > 2**100)) {
+        document.getElementById(`div${id}`).innerHTML = `<h5>Resultados: Error <br> Soma: Error</h5>`;
+        if (numLados == "" || qtdDados == "") {
+            alert("Um dos campos está vazio");
+        } else if (numLados <= 1) {
+            alert("O número de lados do dado tem que ser maior que 1");
+        } else if (qtdDados < 1) {
+            alert("A quantidade de rolagens do dado tem que ser pelo menos 1");
+        } else {
+            alert("Um dos campos tem valor muito alto")
+        }
+        return;
+    }
+
     let resultados = [];
     for (let i = 0; i < qtdDados; i++) {
-        let num = 1;
-        while (num == 1) {
+        let num = Math.floor(Math.random() * numLados) + 1;
+        while (num == numRetirado) {
             num = Math.floor(Math.random() * numLados) + 1;
         }
         resultados.push(num);
@@ -36,5 +61,5 @@ function rolarDados(id) {
 
     let somaArray = resultados.reduce((a, b) => a + b, 0);
 
-    document.getElementById(`div${id}`).innerHTML = `<h5>Resultados: ${resultados.join(", ")} <br> Soma: ${somaArray}</h5>`
+    document.getElementById(`div${id}`).innerHTML = `<h5>Resultados: ${resultados.join(", ")} <br> Soma: ${somaArray}</h5>`;
 }
