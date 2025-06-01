0e2ced3a89ca092c438d0fdd153786eb71ab00db
brasilicioh <brasiliciohcr@gmail.com>
2025-05-31 22:52:30 -0300

    commitar pq tá codando normal

--- JS/main.js
+++ JS/main.js
@@ -1,31 +1,34 @@
 let count = 0;
 
 function addDado() {
-    if (count == 0) {
-        document.getElementById("rolagens").innerHTML += `<br>
-        <div class="input-group mb-3">
-            <div class="input-group-prepend">
-                <span class="input-group-text" id="inputGroup-sizing-default">Número a ser retirado</span>
-            </div>
-            <input id="numRetirado" type="number" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
-        </div>`;
-    }
-    document.getElementById("rolagens").innerHTML += `<div id="botoesDados"><br>  
+    document.getElementById("rolagens").innerHTML += `<br>
+    <div class="input-group mb-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text" id="inputGroup-sizing-default">Número a ser removido</span>
+        </div>
+        <input placeholder="Opcional" id="numRetirado${count}" type="number" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+    </div>
     <div class="input-group">
         <div class="input-group-prepend">
             <span class="input-group-text" id="inputGroup-sizing-default">Lados do Dado</span>
         </div>
         <input id="numLados${count}" type="number" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
     </div>
-    <div class="input-group mb-1">
+    <div class="input-group">
         <div class="input-group-prepend">
             <span class="input-group-text" id="inputGroup-sizing-default">Qtd de Dados</span>
         </div>
         <input id="qtdDados${count}" type="number" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
     </div>
+    <div class="input-group mb-1">
+        <div class="input-group-prepend">
+            <span class="input-group-text" id="inputGroup-sizing-default">Modificações na soma total</span>
+        </div>
+        <input placeholder="Opcional" id="modificacao${count}" type="number" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+    </div>
     <button type="button" class="btn btn-secondary" id="${count}" onclick="rolarDados(this.id)">Jogar dados</button>
     <div id="div${count}"></div>
-    <br></div>`;
+    <br>`;
 
     count += 1;
 }
@@ -33,8 +36,9 @@ function addDado() {
 function rolarDados(id) {
     const numLados = Number(document.getElementById(`numLados${id}`).value);
     const qtdDados = Number(document.getElementById(`qtdDados${id}`).value);
+    const modificacao = Number(document.getElementById(`modificacao${id}`).value);
 
-    const numRetirado = Number(document.getElementById(`numRetirado`).value);
+    const numRetirado = Number(document.getElementById(`numRetirado${id}`).value);
 
     if ((numLados == "" || numLados <= 1) || (qtdDados == "" || qtdDados < 1) || (numLados > 2**100 || qtdDados > 2**100)) {
         document.getElementById(`div${id}`).innerHTML = `<h5>Resultados: Error <br> Soma: Error</h5>`;
@@ -45,7 +49,7 @@ function rolarDados(id) {
         } else if (qtdDados < 1) {
             alert("A quantidade de rolagens do dado tem que ser pelo menos 1");
         } else {
-            alert("Um dos campos tem valor muito alto")
+            alert("Um dos campos tem valor muito alto");
         }
         return;
     }
@@ -59,7 +63,7 @@ function rolarDados(id) {
         resultados.push(num);
     }
 
-    let somaArray = resultados.reduce((a, b) => a + b, 0);
+    let somaArray = resultados.reduce((a, b) => a + b, 0) + modificacao;
 
     document.getElementById(`div${id}`).innerHTML = `<h5>Resultados: ${resultados.join(", ")} <br> Soma: ${somaArray}</h5>`;
 }
