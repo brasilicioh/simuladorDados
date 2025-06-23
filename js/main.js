cd5f2c17249080e7d081f607d8bd55e42dfcae37
brasilicioh <brasiliciohcr@gmail.com>
2025-06-23 00:39:16 -0300

    tudo agora tem addeventlistener

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
+const allArrays = [];
 var count = 0;
+var somaAllArrays = 0;
 
 document.querySelector('.btnAddDado').addEventListener('click', () => {
     document.getElementById("rolagens").innerHTML += `<br>
@@ -27,7 +29,7 @@ document.querySelector('.btnAddDado').addEventListener('click', () => {
         </div>
         <input id="qtdDados${count}" type="number" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
     </div>
-    <button style="margin: 0.3em" type="button" class="btn btn-secondary" id="${count}" onclick="rolarDados(this.id)">Jogar dados</button>
+    <button style="margin: 0.3em" type="button" class="btn btn-secondary rolarDados" data-id="${count}"">Jogar dados</button>
     <div id="div${count}"></div>
     <br>`;
 
@@ -78,3 +80,64 @@ document.querySelectorAll(".botoesSom").forEach(item => {
         })
     });
 });
+
+document.getElementById("rolagens").addEventListener("click", (item) => {
+    rolarDados(item.target.closest(".rolarDados").dataset.id);
+})
+
+function rolarDados(id) {
+    const numLados = Number(document.getElementById(`numLados${id}`).value);
+    const qtdDados = Number(document.getElementById(`qtdDados${id}`).value);
+    const modificacaoAll = Number(document.getElementById(`modificacaoTotal${id}`).value);
+    const modificacaoInd = Number(document.getElementById(`modificacaoIndividual${id}`).value);
+
+    const numRetirado = Number(document.getElementById(`numRetirado${id}`).value);
+
+    if ((numLados == "" || numLados <= 1) || (qtdDados == "" || qtdDados < 1) || (numLados > 2**100 || qtdDados > 2**100)) {
+        document.getElementById(`div${id}`).innerHTML = ``;
+        allArrays[id] = 0;
+        somaAllArrays = allArrays.reduce((a, b) => a + b, 0);
+        document.getElementById("somaAllDados").innerText = `Soma todos dados atuais: ${somaAllArrays}`;
+        if (numLados == "" || qtdDados == "") {
+            alert("Um dos campos está nulo");
+        } else if (numLados <= 1) {
+            alert("O número de lados do dado tem que ser maior que 1");
+        } else if (qtdDados < 1) {
+            alert("A quantidade de rolagens do dado tem que ser pelo menos 1");
+        } else {
+            alert("Um dos campos tem valor muito alto");
+        }
+        return;
+    }
+
+    const resultados = [];
+    for (let i = 0; i < qtdDados; i++) {
+        let num = Math.floor(Math.random() * numLados) + 1;
+        while (num == numRetirado) {
+            num = Math.floor(Math.random() * numLados) + 1;
+        }
+        resultados.push(num + modificacaoInd);
+    }
+
+    const somaArray = resultados.reduce((a, b) => a + b, 0) + modificacaoAll;
+    allArrays.splice(Number(id), 1, somaArray);
+    somaAllArrays = allArrays.reduce((a, b) => a + b, 0);
+
+    const saida = resultados.map(num => {
+        if (num == 1 + modificacaoInd) {
+            return `<span style="color: red">${num}</span>`;
+        } else if (num == numLados + modificacaoInd) {
+            return `<span style="color: green">${num}</span>`;
+        } else {
+            return num;
+        }
+    });
+
+    document.getElementById(`div${id}`).innerHTML = `<h5>Resultados: ${saida.join(", ")} <br> Soma: ${somaArray}</h5>`;
+    document.getElementById("somaAllDados").style.display = "";
+    document.getElementById("somaAllDados").innerText = `Soma todos dados atuais: ${somaAllArrays}`;
+    
+    const audioSom = document.getElementById("audioDado");
+    audioSom.currentTime = 0;
+    audioSom.play();
+}
