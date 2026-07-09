c8b11e8707e58c5546ebe98902e257cc6c45acd3
brasilicioh <brasiliciohcr@gmail.com>
2026-07-09 13:20:33 -0300

    feat: add random audio function

--- js/core/som.js
+++ js/core/som.js
@@ -1,5 +1,12 @@
+const audios = ["sound1.mp4", "sound2.mp4", "sound3.mp4", "sound4.mp4", "sound5.mp4", "sound6.mp4", "sound7.mp4", "sound8.mp4", "sound9.mp4", "sound10.mp4", "sound11.mp4", "sound12.mp4", "sound13.mp4"]
+
+const randomAudio = () => {
+    return audios[Math.floor(Math.random() * audios.length)];
+}
+
 export function tocarOSom(element) {
-    const nomeSom = element.dataset.som;
+    const isRandom = element.dataset.som === "random";
+    const nomeSom = !isRandom ? element.dataset.som : randomAudio();
     document.getElementById("tocarOSom").innerHTML = `<audio id="audio" src="sons/${nomeSom}" autoplay></audio>`;
     document.querySelectorAll(".definirSom").forEach(() => {
         document.getElementById("tocarOSomDado").innerHTML = `<audio id="audioDado" src="sons/${nomeSom}"></audio>`;
